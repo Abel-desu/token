5d1712294a5c87ca3d09bb2eaf9b321326f3f6ff
Cent <80540398+cent-dxv@users.noreply.github.com>
2024-08-12 22:03:36 +0300

    test this code
     ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
-"use client";
+// "use client";
 
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import { getOneDriveAccessToken } from "../lib/onedrive";
 
 function Page() {
-  const [token, setToken] = useState("");
 
-  useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const { token } = await getOneDriveAccessToken();
-        setToken(token);
-      } catch (error) {
-        console.error("Error fetching token:", error);
-      }
-    };
+  const { token } = await getOneDriveAccessToken();
 
-    fetchToken();
-  }, []);
+  const raw_token = await getOneDriveAccessToken();
+
+  console.log( "/n/n/n/n" , "token :" ,token , "raw responded" ,raw_token)
+  // const [token, setToken] = useState("");
+
+  // useEffect(() => {
+  //   const fetchToken = async () => {
+  //     try {
+  //       const { token } = await getOneDriveAccessToken();
+  //       setToken(token);
+  //     } catch (error) {
+  //       console.error("Error fetching token:", error);
+  //     }
+  //   };
+
+    // fetchToken();
+  // }, []);
 
   return (
     <div>
